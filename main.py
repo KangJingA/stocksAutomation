f075d5ddba3f3fcd888972e10236256b973f7ba2
Nicholas Ang <nicholaskangjing.ang@dxc.com>
2020-11-27 18:22:41 +0800

    update analysis

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # get-all-tickers - helps compile list of stocks
 import yfinance as yf, pandas as pd
 import shutil, os, time, glob, smtplib, ssl
+from functools import reduce
 
 
 ### Extracting stocks of interest 
@@ -16,7 +17,12 @@ totalApiCalls = 0
 stockFailure = 0
 stocksNotImported = 0
 
-##os.mkdir("..\Daily_Stock_Report\Stocks")
+
+if os.path.exists("dailyStockReport\\stocks"):
+    shutil.rmtree("dailyStockReport\\stocks")
+
+
+os.mkdir("dailyStockReport\\stocks")
 
 for i in tickers:
     
@@ -26,7 +32,7 @@ for i in tickers:
             stock = i
             tempData = yf.Ticker(stock)
             historicalData = tempData.history(period="max")
-            historicalData.to_csv(stock+".csv") 
+            historicalData.to_csv("dailyStockReport\\stocks\\" +stock+".csv") 
             time.sleep(2) # pause loop for 2 seconds
             totalApiCalls += 1
             stockFailure = 0
@@ -42,12 +48,51 @@ for i in tickers:
             totalApiCalls += 1
             stockFailure += 1
 
-        else:
-            break
              
 print("Amount of stocks successfully imported: " + str(len(tickers) - stocksNotImported))
 print("Amoount of stocks failed import: " + str(stocksNotImported))
             
-## Perform OBV Stock Analysis 
+## Perform OBV Stock Analysis from extracted data
 
-           
+# grab list of file paths
+list_files = (glob.glob("dailyStockReport\\stocks\\*.csv"))
+OBV_data = []
+tradingDays = 10 # number of trading days to observe
+
+for file in list_files:
+
+    # read each file and grab the data from the last 10 days
+    stockData = pd.read_csv(file).tail(tradingDays)
+    positiveMove, negativeMove = [], []
+
+    for i in range(tradingDays):
+        
+        if stockData.iloc[i,1] < stockData.iloc[i,4]: # open vs close
+           positiveMove.append(i) #grab the days
+
+        elif stockData.iloc[i,1] > stockData.iloc[i,4]:
+            negativeMove.append(i)
+        
+    # update OBV value
+    OBV_value_pos = [stockData.iloc[i,5]/stockData.iloc[i,1] for i in positiveMove]
+    OBV_value_neg = [stockData.iloc[i,5]/stockData.iloc[i,1] for i in negativeMove]
+    OBV_value = sum(OBV_value_pos) - sum(OBV_value_neg)
+    print(OBV_value) 
+    Stock_Name = ((os.path.basename(file)).split(".csv")[0])  # Get the name of the current stock we are analyzing
+    OBV_data.append([Stock_Name, OBV_value])  # Add the stock name and OBV value to the new_data list
+
+final_df = pd.DataFrame(OBV_data, columns = ["Ticker", "OBV Value"])
+
+final_df["Stocks_Ranked"] = final_df["OBV Value"].rank(ascending = False)  # Rank the stocks by their OBV_Values
+final_df.sort_values("OBV Value", inplace = True, ascending = False)  # Sort the ranked stocks
+final_df.to_csv("dailyStockReport\\OBV_Ranked.csv", index = False)  # Save the dataframe to a csv without the index column
+
+
+
+
+
+
+
+
+
+
